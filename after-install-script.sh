0f88cce609f3557a40f9b3a13f9d504129e57381
amich1 <49416568+amich1@users.noreply.github.com>
2019-11-12 02:50:31 +0000

    added after-install-script.sh

---
+++ after-install-script.sh
@@ -0,0 +1,3 @@
+# set of commands to be executed on a fresh install of debian
+# suppose to be ran as root
+apt install build-essential sudo neovim rxvt-unicode zsh awesome xorg feh xpdf xfe git neofetch lightdm wget firefox-esr pnmixer alsa-tools alsa-utils net-tools network-manager wicd
